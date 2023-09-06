62899b9344375aa0131e4e5abdf36804541ba7d1
void-zenith <zenithrajbhandari@gmail.com>
2023-09-06 12:18:16 -0400

    final demo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Hi!👋</Text>
+      <Text style={styles.subheader}>
+        I am <Text style={styles.highlightedText}>zenith rajbhandari. </Text>
+      </Text>
+      <Text>This is my first app.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 70,
+    color: "red",
+    fontWeight: "900",
+    textTransform: "uppercase",
+  },
+  subheader: {
+    textAlign: "center",
+    fontSize: 30,
+  },
+  highlightedText: {
+    fontSize: 36,
+    textTransform: "uppercase",
+    fontWeight: "700",
+    textDecorationLine: "underline",
   },
 });
